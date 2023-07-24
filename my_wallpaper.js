583ae8e27590f800c1f22e3b3b55111a85696580
baolong2005 <baolongvt2005@gmail.com>
2023-07-24 12:14:25 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,7 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
-
+let cir_width  = 120;
+let cir_height = cir_width;
+//let Night_background = color(233, 245, 2);
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -15,9 +15,26 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(45, 12, 48); // dark purple 
+  
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+ 
+  fill(233, 245, 2); // Yellow
+  strokeWeight(1);
+  stroke(233, 245, 2); // skyline color
+  line(0,150,200,150); // skyline
+  strokeWeight(0);
+  arc(100,150,cir_width,cir_height,180,360); // moon
+  strokeWeight(3);
+  stroke(233, 245, 2);
+  line(50,158,150,158);
+  strokeWeight(2);
+  line(70,166,130,166);
+  strokeWeight(2);
+  line(90,174,110,174);
+
+
+ 
 }
