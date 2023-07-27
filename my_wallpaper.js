2f43b02df28632368008a3fd00f975c84ec12fdd
baolong2005 <baolongvt2005@gmail.com>
2023-07-27 21:58:45 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,8 @@
 //your parameter variables go here!
 let circle_posX = 100;
 let circle_posY = 100;
+var circle_scaleW = 80;
+var circle_scaleH = 80;
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -19,32 +21,33 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+ 
   strokeWeight(5)
   stroke(255, 149, 0) // Orange
   fill(245, 234, 186) // Orange inner color
     //Orange
-  circle(circle_posX,circle_posY,100,100)
-  strokeWeight(1)
+  circle(circle_posX,circle_posY,circle_scaleW,circle_scaleH);
+  strokeWeight(1);
   fill(255, 149, 0) // Orange
-  arc(circle_posX,circle_posY,90,90,0,67);
-  arc(circle_posX,circle_posY,90,90,72,139);
-  arc(circle_posX,circle_posY,90,90,144,211);
-  arc(circle_posX,circle_posY,90,90,216,283);
-  arc(circle_posX,circle_posY,90,90,288,355);
+  arc(circle_posX,circle_posY,circle_scaleW - 10,circle_scaleH - 10,0,67);
+  arc(circle_posX,circle_posY,circle_scaleW - 10,circle_scaleH - 10,72,139);
+  arc(circle_posX,circle_posY,circle_scaleW - 10,circle_scaleH - 10,144,211);
+  arc(circle_posX,circle_posY,circle_scaleW - 10,circle_scaleH - 10,216,283);
+  arc(circle_posX,circle_posY,circle_scaleW - 10,circle_scaleH - 10,288,355);
 
   fill(252, 163, 38);  
-  arc(circle_posX,circle_posY,80,80,0,67);
-  arc(circle_posX,circle_posY,80,80,72,139);
-  arc(circle_posX,circle_posY,80,80,144,211);
-  arc(circle_posX,circle_posY,80,80,216,283);
-  arc(circle_posX,circle_posY,80,80,288,355);
+  arc(circle_posX,circle_posY,circle_scaleW - 20,circle_scaleH - 20,0,67);
+  arc(circle_posX,circle_posY,circle_scaleW - 20,circle_scaleH - 20,72,139);
+  arc(circle_posX,circle_posY,circle_scaleW - 20,circle_scaleH - 20,144,211);
+  arc(circle_posX,circle_posY,circle_scaleW - 20,circle_scaleH - 20,216,283);
+  arc(circle_posX,circle_posY,circle_scaleW - 20,circle_scaleH - 20,288,355);
   fill(245, 234, 186); 
   stroke(252, 163, 38);
   circle(circle_posX,circle_posY,15,15);
 
-  strokeWeight(0);
-  ellipse(110,90,10,5);
-  rotateZ(ellipse()/1000)
+  //strokeWeight(0);
+  //ellipse(110,90,10,5);
+  
   
 
  
