b9097eb32700121f1c3863f7ac1af7ea5aa919b0
baolong2005 <baolongvt2005@gmail.com>
2023-07-30 23:35:13 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,6 @@
 //your parameter variables go here!
-let circle_posX = 100;
-let circle_posY = 100;
+var circle_posX = 100;
+var circle_posY = 100;
 var circle_scaleW = 100;
 var circle_scaleH = 100;
 
@@ -27,7 +27,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   fill(245, 234, 186) // Orange inner color
     //Orange
   circle(circle_posX,circle_posY,circle_scaleW,circle_scaleH);
-  strokeWeight(1);
+  strokeWeight(0.3);
   fill(255, 149, 0) // Orange
   arc(circle_posX,circle_posY,circle_scaleW - 10,circle_scaleH - 10,0,67);
   arc(circle_posX,circle_posY,circle_scaleW - 10,circle_scaleH - 10,72,139);
@@ -35,18 +35,18 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   arc(circle_posX,circle_posY,circle_scaleW - 10,circle_scaleH - 10,216,283);
   arc(circle_posX,circle_posY,circle_scaleW - 10,circle_scaleH - 10,288,355);
 
-  fill(252, 163, 38);  
+  fill(252, 163, 38);  // Lighter inner Orange
   arc(circle_posX,circle_posY,circle_scaleW - 20,circle_scaleH - 20,0,67);
   arc(circle_posX,circle_posY,circle_scaleW - 20,circle_scaleH - 20,72,139);
   arc(circle_posX,circle_posY,circle_scaleW - 20,circle_scaleH - 20,144,211);
   arc(circle_posX,circle_posY,circle_scaleW - 20,circle_scaleH - 20,216,283);
   arc(circle_posX,circle_posY,circle_scaleW - 20,circle_scaleH - 20,288,355);
-  fill(245, 234, 186); 
+  fill(245, 234, 186);
+  strokeWeight(3); 
   stroke(252, 163, 38);
   circle(circle_posX,circle_posY,15,15);
 
-  //strokeWeight(0);
-  //ellipse(110,90,10,5);
+
   
   
 
